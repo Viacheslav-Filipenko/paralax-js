6c74d0b91e708505306c3fe91de00462bbdfe628
Viacheslav Filipenko <filipenko.lavik@gmail.com>
2019-05-27 18:30:27 +0300

    add angle between model and camera

--- app/classes/Object3D.js
+++ app/classes/Object3D.js
@@ -5,12 +5,61 @@ export default class Object3D {
 	constructor(obj) {
 		this.graphic = new Graphic();
 		this.position = new Vector(obj.position, 0);
-		this.rotation = new Vector(obj.rotation, 0);
-		this.scale = new Vector(obj.scale, 1);
-		this._matrix = this.graphic.trasformation(this);
+
+		this._rotationMatrix = this.graphic.rotate(new Vector(obj.rotation, 0));
+		this._translationMatrix = this.graphic.translate(new Vector(obj.position, 0));
+		this._scaleMatrix = this.graphic.scale(new Vector(obj.scale, 1));
+
+		this._matrix = this.graphic.trasform(this);
+
+		this.ViewAngleChange = () => {};
+
+		this.ViewAngle = { x: 0, y: 0, z: 0 };
 	}
 
 	get matrix() {
 		return this._matrix;
+	}
+
+	onViewAngleChange(callback) {
+		this.ViewAngleChange = callback;
+	}
+
+	getPosition() {
+		return this.graphic.getPosition(this._matrix);
+	}
+
+	getAngleToCamera(camera) {
+		const x = this.getAngleToCameraX(camera);
+		const y = this.getAngleToCameraY(camera);
+		const z = this.getAngleToCameraZ(camera);
+
+		this.ViewAngle = { x, y, z };
+		console.log(this.ViewAngle);
+
+		return this.ViewAngle;
+	}
+
+	getAngleToCameraX(camera) {
+		const { right } = this.graphic.getDirection(this._rotationMatrix);
+		const cameraRight = this.graphic.getDirection(camera._rotationMatrix).right;
+		const angleX = Vector.getAngleBetweenTwoVectors(right, cameraRight);
+
+		return Math.ceil(angleX);
+	}
+	getAngleToCameraY(camera) {
+		const { up } = this.graphic.getDirection(this._rotationMatrix);
+		const cameraUp = this.graphic.getDirection(camera._rotationMatrix).up;
+		const angleY = Vector.getAngleBetweenTwoVectors(up, cameraUp);
+
+		return Math.ceil(angleY);
+	}
+
+	getAngleToCameraZ(camera) {
+		const { forward } = this.graphic.getDirection(this._rotationMatrix);
+		const cameraForward = this.graphic.getDirection(camera._rotationMatrix).forward;
+		const angleZ = Vector.getAngleBetweenTwoVectors(forward, cameraForward);
+
+		return Math.ceil(angleZ);
 	}
 }
